95de9953ff30b0abdb3aa1018c104f9ea160342c
zulus23 <zulus23@gmail.com>
2019-12-18 16:57:32 +0300

    change class role

--- app/authentication/model/authorizations/Roles.scala
+++ app/authentication/model/authorizations/Roles.scala
@@ -1,22 +1,20 @@
 package authentication.model.authorizations
 
 import authentication.model.User
-import authentication.utils.SessionEnv
 import com.mohiva.play.silhouette.api.Authorization
+import com.mohiva.play.silhouette.impl.authenticators.SessionAuthenticator
 import play.api.mvc.Request
 
 import scala.concurrent.Future
 
-/*
-case class withRole(role:Role) extends Authorization[User,SessionEnv] {
-  override def isAuthorized[B](user: User, authenticator: SessionEnv)(implicit request: Request[B]): Future[Boolean] = user.roles match {
+case class withRole(role:Role) extends Authorization[User,SessionAuthenticator] {
+  override def isAuthorized[B](user: User, authenticator: SessionAuthenticator)(implicit request: Request[B]): Future[Boolean] = user.roles match {
     case list: Seq[Role] => Future.successful(list.contains(role))
     case _ => Future.successful(false)
   }
 
 
 }
-*/
 trait Role {
 
 }
