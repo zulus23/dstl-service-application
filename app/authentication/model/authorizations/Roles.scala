860957d7857bd50a94317a5f3c8c55cf734ce035
zulus23 <zulus23@gmail.com>
2019-12-18 16:51:26 +0300

    change class role

--- app/authentication/model/authorizations/Roles.scala
+++ app/authentication/model/authorizations/Roles.scala
@@ -7,7 +7,7 @@ import play.api.mvc.Request
 
 import scala.concurrent.Future
 
-
+/*
 case class withRole(role:Role) extends Authorization[User,SessionEnv] {
   override def isAuthorized[B](user: User, authenticator: SessionEnv)(implicit request: Request[B]): Future[Boolean] = user.roles match {
     case list: Seq[Role] => Future.successful(list.contains(role))
@@ -16,7 +16,7 @@ case class withRole(role:Role) extends Authorization[User,SessionEnv] {
 
 
 }
-
+*/
 trait Role {
 
 }
